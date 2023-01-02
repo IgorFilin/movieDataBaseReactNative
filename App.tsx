8d32cca8a6db2ff31576e109a3725ae082316ed8
IgorFilin <filinigor@yandex.ru>
2023-01-02 15:13:50 +0300

    feat:create SearchContent component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
-import {StyleSheet, View} from 'react-native';
-import {Button, TextInput} from "react-native-paper";
-import {useState} from "react";
 import {Provider} from "react-redux";
-import {store, useAppDispatch} from "./src/BLL/store/store";
-import {contentThunk} from "./src/BLL/thunk/contentThunk";
+import {store} from "./src/BLL/store/store";
+import {SearchContent} from "./src/UI/SeacrhContent";
 
 export default function defaultApp() {
     return (
@@ -14,46 +11,11 @@ export default function defaultApp() {
 }
 
 function App() {
-    const [text, setText] = useState("");
-
-    const dispatch = useAppDispatch()
-
-    const searchFilmsHandler = () => {
-        dispatch(contentThunk({title: text}))
-    }
-
     return (
-
-        <View style={styles.container}>
-            <View style={styles.searchContainer}>
-                <TextInput label="Search films"
-                           value={text}
-                           onChangeText={text => setText(text)}/>
-
-                <Button style={styles.buttonSend} mode="contained" onPress={searchFilmsHandler}>
-                    Send Search
-                </Button>
-            </View>
-        </View>
-
-    );
+        <>
+            <SearchContent/>
+        </>
+    )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: '#fff'
-    },
-    buttonSend: {
-        width: '100%',
-        marginTop: 10,
-        alignSelf: 'center',
-    },
-    searchContainer: {
-        marginTop: 100,
-        display: 'flex',
-        justifyContent: 'center',
-        width: '40%'
-    }
-});
+
---
+++ src/UI/SeacrhContent.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {StyleSheet, View} from "react-native";
+import {Button, TextInput} from "react-native-paper";
+import {useAppDispatch} from "../BLL/store/store";
+import {contentThunk} from "../BLL/thunk/contentThunk";
+
+export const SearchContent = () => {
+
+    const [text, setText] = useState("");
+
+    const dispatch = useAppDispatch()
+
+    const searchFilmsHandler = () => {
+        dispatch(contentThunk({title: text}))
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchContainer}>
+                <TextInput label="Search films"
+                           value={text}
+                           onChangeText={text => setText(text)}/>
+
+                <Button style={styles.buttonSend} mode="contained" onPress={searchFilmsHandler}>
+                    Send Search
+                </Button>
+            </View>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    buttonSend: {
+        width: '100%',
+        marginTop: 10,
+        alignSelf: 'center',
+    },
+    searchContainer: {
+        marginTop: 100,
+        display: 'flex',
+        justifyContent: 'center',
+        width: '40%'
+    }
+});
